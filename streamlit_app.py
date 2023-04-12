1f6c71a6a531bda05e2f861d9d499f78c3354cd1
JoFreegard <56542920+JoFreegard@users.noreply.github.com>
2023-04-12 21:21:15 +0100

    Update streamlit_app.py
    Added text entry box for user to add a fruit to the list and code to display the fruit they added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# allow the user to add a fruit to the list
+fruit_to_add = streamlit.text_input('What fruit would you like to add to the list?', 'Enter fruit here')
+streamlit.write('Thanks for adding', fruit_to_add)
+
